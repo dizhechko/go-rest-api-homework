7f5bdec4a79df3655f8afa2d44a21084522ea213
Dizhechko Nataly <dizhechko.nata@yandex.ru>
2024-05-17 16:35:26 +0500

    функция GET,Get ID, DELETE проверена

--- precode.go
+++ precode.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -41,12 +43,102 @@ var tasks = map[string]Task{
 
 // Ниже напишите обработчики для каждого эндпоинта
 // ...
+func getTask(w http.ResponseWriter, r *http.Request) {
+	// сериализуем данные из слайса artists
+	/*	resp, err := json.MarshalIndent(tasks, " ", "   ")*/
+	resp, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// в заголовок записываем тип контента, у нас это данные в формате JSON
+	w.Header().Set("Content-Type", "application/json")
+	// так как все успешно, то статус OK
+	w.WriteHeader(http.StatusOK)
+	// записываем сериализованные в JSON данные в тело ответа
+	w.Write(resp)
+}
+
+/*
+Для тестирования использовано
+curl http://localhost:8080/tasks --include --header "Content-Type: application/json" --request "POST" --data '{"id":"3","description":"конец","note":"коне",ц","applications":["VS Code","Terminal","git","Postman"]}'
+*/
+func postTask(w http.ResponseWriter, r *http.Request) {
+	var taskPost Task
+	var buf bytes.Buffer
+
+	_, err := buf.ReadFrom(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err = json.Unmarshal(buf.Bytes(), &taskPost); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	tasks[taskPost.ID] = taskPost
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+}
+
+func getTaskId(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	tskid, ok := tasks[id]
+	if !ok {
+		http.Error(w, "Не найдено", http.StatusNoContent)
+		return
+	}
+
+	resp, err := json.Marshal(tskid)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+func delTaskId(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	tskid, ok := tasks[id]
+	if !ok {
+		http.Error(w, "Не найдено", http.StatusNoContent)
+		return
+	} else {
+		delete(tasks, id)
+	}
+
+	resp, err := json.Marshal(tskid)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
 
 func main() {
 	r := chi.NewRouter()
 
 	// здесь регистрируйте ваши обработчики
 	// ...
+	// регистрируем в роутере эндпоинт `/tasks` с методом GET, для которого используется обработчик `getTask`
+	r.Get("/tasks", getTask)
+	// регистрируем в роутере эндпоинт `/tasks` с методом POST, для которого используется обработчик `postTask`
+	r.Post("/tasks", postTask)
+	// регистрируем в роутере эндпоинт `/task/{id}` с методом GET, для которого используется обработчик `getTaskId`
+	r.Get("/tasks/{id}", getTaskId)
+	// регистрируем в роутере эндпоинт `/task/{id}` с методом DELETE, для которого используется обработчик `delTaskId`
+	r.Get("/tasks/{id}", delTaskId)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
